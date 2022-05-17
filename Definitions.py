68004a29e1795ca45660d1f6266d62fdc9c94ba2
Tyler Rogers <tr508315@ohio.edu>
2022-05-17 16:54:36 -0400

    feat is working

--- Definitions.py
+++ Definitions.py
@@ -1,15 +1,18 @@
 # Here are the Definitions of methods
 import os
 from datetime import datetime
+import time
 
-
-def loopTime(inputTime): 
-    test = inputTime
+def loopPing(inputTime,targets):
+    for y in range(0,inputTime):
+        for curTarget in targets:
+            testPing(targets[curTarget],curTarget)
+        time.sleep(1)
 
 
 def testPing(targetIP,targetName): 
-    response = os.system("ping -c 1 " + targetIP)
-
+    response = os.system("ping -n 1 " + targetIP)
+    failure = "Can't Reach: " + targetName + " Time: " + str(datetime.now().time()) + "\n"
     if response != 0:
         with open("logs.txt", "a") as myfile:
-            myfile.write("Can't Reach: ",targetName, " Time: ",datetime.now().time())
+            myfile.write(failure)
--- Run.py
+++ Run.py
@@ -1,1 +1,9 @@
 # This is where to call the methods defined in Definitions.py
+from Definitions import *
+
+targets = {
+    "Zay": "192.168.0.104",
+    "Google": "www.Google.com"
+}
+
+loopPing(10,targets)
