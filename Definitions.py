e6ca08af763865cd54c21c4bf35b62402789a508
Tyler Rogers <tr508315@ohio.edu>
2022-05-17 16:15:36 -0400

    adding gitignore so logs are not public

--- Definitions.py
+++ Definitions.py
@@ -1,5 +1,7 @@
 # Here are the Definitions of methods
 import os
+from datetime import datetime
+
 
 def loopTime(inputTime): 
     test = inputTime
@@ -9,4 +11,5 @@ def testPing(targetIP,targetName):
     response = os.system("ping -c 1 " + targetIP)
 
     if response != 0:
-        print ("Can't Reach: ",targetName)
+        with open("logs.txt", "a") as myfile:
+            myfile.write("Can't Reach: ",targetName, " Time: ",datetime.now().time())
