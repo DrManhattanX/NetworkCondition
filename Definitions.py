dae244e293927a1b9443847ec18068f42a50a0c6
Tyler Rogers <tr508315@ohio.edu>
2022-05-17 17:20:47 -0400

    changing to be less verbose and clog the terminal

--- Definitions.py
+++ Definitions.py
@@ -2,17 +2,27 @@
 import os
 from datetime import datetime
 import time
+import subprocess
+process = subprocess.Popen(['ping','-c' ,'1', '8.8.8.8'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+returncode = process.returncode
+print(returncode)
+
+
 
 def loopPing(inputTime,targets):
     for y in range(0,int(inputTime/2)):
+        print("loops left" + y)
         for curTarget in targets:
             testPing(targets[curTarget],curTarget)
         time.sleep(1)
 
 
 def testPing(targetIP,targetName): 
-    response = os.system("ping -n 1 " + targetIP)
+    process = subprocess.Popen(['ping','-n' ,'1', targetIP], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    returncode = process.returncode
     failure = "Can't Reach: " + targetName + " Time: " + str(datetime.now().time()) + "\n"
-    if response != 0:
+    if returncode != 0:
         with open("logs.txt", "a") as myfile:
             myfile.write(failure)
--- Run.py
+++ Run.py
@@ -4,8 +4,9 @@ from Definitions import *
 # Target name and IP/address
 targets = {
     "Zay": "192.168.0.104",
-    "Google": "www.Google.com"
+    "Google": "www.Google.com",
+    "Brian": "74.135.15.103"
 }
 
 # input time in (even int)seconds to run and targets
-loopPing(10,targets)
+loopPing(60,targets)
